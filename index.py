e623c5e519f7466d385f19ae6df41ad4f3f40f6c
saurabh.xyz <saurabh@appler.xyz>
2021-08-27 08:55:30 +0530

    add new endpoint to get price infor from zerodha api

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import requests
+import json
 # import opstrat as op
 from flask_cors import CORS
 from jugaad_data.nse import NSELive
@@ -11,21 +12,6 @@ CORS(app)
 @app.route('/')
 def home():
     return 'Home Page Route'
-
-
-@app.route('/about')
-def about():
-    return 'About Page Route'
-
-
-@app.route('/portfolio')
-def portfolio():
-    return 'Portfolio Page Route'
-
-
-@app.route('/contact')
-def contact():
-    return 'Contact Page Route'
 
 
 @app.route('/api/data')
@@ -42,6 +28,27 @@ def strat_op():
         # res = requests.get("https://opstra.definedge.com/api/free/strategybuilder/payoff/{}".format('BANKNIFTY$+50x35400CEx12AUG2021x480.55x0x0&-50x35700CEx12AUG2021x236.22x0x0&+25x35500CEx18AUG2021x603.1x0x0&-50x35800CEx12AUG2021x160.13x0x0&+25x35600CEx18AUG2021x519.9x0x0$2021-08-12$0$0$0'))
         return jsonify(res.json())
 
+@app.route('/api/prices')
+def zero_prices():
+    url = "https://kite.zerodha.com/oms/margins/orders"
+    # /api/op?s=BANKNIFTY&q=25&st=36000&ty=CE&ex=21AUG2021&si=BUY&ot=MARKET&p=NRML&v=regular
+    tradingsymbol = "{}{}{}{}".format(request.args['s'], request.args['ex'], request.args['st'], request.args['ty'])
+    payload = [
+	{
+		"exchange": "NFO",
+		"tradingsymbol": tradingsymbol,
+		"transaction_type": request.args['si'],
+		"variety": request.args['v'],
+		"product": request.args['p'],
+		"order_type": request.args['ot'],
+		"quantity": float(request.args['q'])
+	}
+    ]
+    # FIXME: the header shall be auto populated post zerodha sign-in and shall not be an input. OKAY for local setups, not for PRODUCTionalizing this.
+    # passing vulnerability to the consumer of this API
+    headers = {'authorization': 'enctoken {}'.format(request.headers['x-tok'])}
+    response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+    return jsonify(response.json())
 
 
 @app.route('/api/oc')
